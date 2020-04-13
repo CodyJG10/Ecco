bb97e2257adb329cc63a6598be59a17d0100ea80
AcePilot10 <codyjg10@gmail.com>
2020-04-13 10:41:02 -0400

    Added unit testing

--- Ecco.Tests/ApiTests.cs
+++ Ecco.Tests/ApiTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Ecco.Api;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,25 +8,74 @@ namespace Ecco.Tests
     [TestClass]
     public class ApiTests
     {
-        private DatabaseManager _db;
+        #region Fields
+        private readonly DatabaseManager _db;
+        private const string id = "baf2f30a-671b-4fa7-91c8-837cb4e1e770";
+        #endregion
 
-        [TestMethod]
-        public async void TestLogin()
+        #region Init
+        
+        public ApiTests()
+        {
+            _db = new DatabaseManager();
+            _db.SetUrl("https://ecco-space.azurewebsites.net");
+            Login();
+        }
+
+        private void Login()
         {
             string username = "codyjg10@gmail.com";
             string password = "Airplane10";
-            string url = "https://localhost:44355";
-            _db = new DatabaseManager();
-            bool loginResult = await _db.Login(username, password);
-            string message = loginResult ? "Login Succeeded" : "Login Failed";
-            Assert.IsTrue(loginResult, message);
+            _db.Login(username, password).GetAwaiter().GetResult();
+        }
+
+        #endregion
+
+        #region Cards
+
+        [TestMethod]
+        public void GetCards()
+        {
+            var cards = _db.GetCards().GetAwaiter().GetResult();
+            Assert.IsNotNull(cards, "Retrieved " + cards.ToList().Count + " cards.");
         }
 
         [TestMethod]
-        public async void TestCards()
+        public void GetMyCards()
         {
-            var cards = await _db.GetCards();
-            Assert.IsNotNull(cards);
+            var cards = _db.GetMyCards(id).GetAwaiter().GetResult();
+            Assert.IsNotNull(cards, "Retrieved " + cards.ToList().Count + " cards.");
         }
+
+        #endregion
+
+        #region Connections
+
+        [TestMethod]
+        public void GetMyConnections()
+        {
+            var connections = _db.GetMyConnections(new Guid(id)).GetAwaiter().GetResult().ToList();
+            Assert.IsNotNull(connections, "Retrieved " + connections.Count + " results");
+        }
+
+        [TestMethod]
+        public void GetMyPendingConnections()
+        {
+            var connections = _db.GetMyPendingConnections(new Guid(id)).GetAwaiter().GetResult().ToList();
+            Assert.IsNotNull(connections, "Retrieved " + connections.Count + " results");
+        }
+
+        #endregion
+
+        #region Templates
+
+        [TestMethod]
+        public void GetTemplates()
+        {
+            var templates = _db.GetTemplates().GetAwaiter().GetResult();
+            Assert.IsNotNull(templates, "Retrieved " + templates.Count + " results");
+        }
+
+        #endregion
     }
 }
---
+++ Ecco.Tests/AuthTests.cs
@@ -0,0 +1,44 @@
+﻿using Ecco.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecco.Tests
+{
+    [TestClass]
+    public class AuthTests
+    {
+        private IDatabaseManager _db;
+
+        public AuthTests()
+        {
+            _db = new DatabaseManager();
+            _db.SetUrl("https://ecco-space.azurewebsites.net");
+        }
+
+        [TestMethod]
+        public void LoginWithValidCredentials()
+        {
+            string username = "codyjg10@gmail.com";
+            string password = "Airplane10";
+            bool loginResult = _db.Login(username, password).GetAwaiter().GetResult();
+            string message = loginResult ? "Login Succeeded" : "Login Failed";
+            Assert.IsTrue(loginResult, message);
+        }
+
+        [TestMethod]
+        public void LoginWithInvalidCredentials()
+        {
+            string username = "codyjg10@gmail.com";
+            string password = "NotMyPassword";
+            _db = new DatabaseManager();
+            _db.SetUrl("https://ecco-space.azurewebsites.net");
+            bool loginResult = _db.Login(username, password).GetAwaiter().GetResult();
+            string message = loginResult ? "Login Succeeded" : "Login Failed";
+            Assert.IsFalse(loginResult, message);
+        }
+    }
+}
