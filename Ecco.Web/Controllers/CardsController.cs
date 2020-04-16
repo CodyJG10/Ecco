2a24ef307a86dac2453a0399054f5c60410da699
AcePilot10 <codyjg10@gmail.com>
2020-04-15 21:52:10 -0400

    updated web

--- Ecco.Web/Controllers/CardsController.cs
+++ Ecco.Web/Controllers/CardsController.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.Authorization;
 namespace Ecco.Web.Controllers
 {
     [Route("{controller}")]
-    [Authorize(Roles = "Moderator,Admin")]
     public class CardsController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -22,141 +21,11 @@ namespace Ecco.Web.Controllers
             _context = context;
         }
 
-        // GET: Cards
-        [HttpGet("")]
-        [HttpGet("Index")]
-        public async Task<IActionResult> Index()
+        [Route("{id}")]
+        public ActionResult GetCard(int id)
         {
-            return View(await _context.Cards.ToListAsync());
-        }
-
-        // GET: Cards/Details/5
-        [HttpGet("Details/{id}")]
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var card = await _context.Cards
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (card == null)
-            {
-                return NotFound();
-            }
-
-            return View(card);
-        }
-
-        // GET: Cards/Create
-        [HttpGet("Create")]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Cards/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost("Create")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UserId,FullName,JobTitle,Description,CardTitle,Email,Phone,TemplateId,ServiceType")] Card card)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(card);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(card);
-        }
-
-        // GET: Cards/Edit/5
-        [HttpGet("Edit/{id}")]
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var card = await _context.Cards.FindAsync(id);
-            if (card == null)
-            {
-                return NotFound();
-            }
-            return View(card);
-        }
-
-        // POST: Cards/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost("Edit/{id}")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,FullName,JobTitle,Description,CardTitle,Email,Phone,TemplateId,ServiceType")] Card card)
-        {
-            if (id != card.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(card);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CardExists(card.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(card);
-        }
-
-        // GET: Cards/Delete/5
-        [HttpGet("Delete/{id}")]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var card = await _context.Cards
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (card == null)
-            {
-                return NotFound();
-            }
-
-            return View(card);
-        }
-
-        // POST: Cards/Delete/5
-        [HttpPost("Delete/{id}")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var card = await _context.Cards.FindAsync(id);
-            _context.Cards.Remove(card);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool CardExists(int id)
-        {
-            return _context.Cards.Any(e => e.Id == id);
+            var card = _context.Cards.Single(x => x.Id == id);
+            return View("Card", card);
         }
     }
 }
