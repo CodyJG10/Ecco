a003715f9b9642e078096bca11c73b2daca5d428
Cody Gordon <codyjg10@gmail.com>
2020-04-26 04:42:17 -0400

    Configured toolbar items in editor

--- Ecco.Mobile/Ecco.Mobile/Views/Pages/Cards/CreateCardEditor.xaml.cs
+++ Ecco.Mobile/Ecco.Mobile/Views/Pages/Cards/CreateCardEditor.xaml.cs
@@ -1,7 +1,11 @@
-﻿using Ecco.Mobile.Models;
+﻿using Ecco.Entities.CardText;
+using Ecco.Mobile.Models;
 using Ecco.Mobile.ViewModels.Home.Card;
+using Newtonsoft.Json;
+using Syncfusion.SfImageEditor.XForms;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +22,42 @@ namespace Ecco.Mobile.Views.Pages.Cards
         {
             InitializeComponent();
             BindingContext = new CreateCardEditorViewModel(model);
+            ImageEditor.SetToolbarItemVisibility("path,shape,transform,effects", false);
+
+            ImageEditor.ToolbarSettings.ToolbarItems.Add(new HeaderToolbarItem() { Text = "Export" });
+            ImageEditor.ToolbarSettings.ToolbarItemSelected += ToolbarSettings_ToolbarItemSelected; ;
+        }
+
+        private void ToolbarSettings_ToolbarItemSelected(object sender, ToolbarItemSelectedEventArgs e)
+        {
+            if (e.ToolbarItem.Text == "Export")
+            {
+                var saveStream = ImageEditor.SaveEdits();
+                StreamReader reader = new StreamReader(saveStream);
+                string json = reader.ReadToEnd();
+
+                TextObjectList list = TextObjectList.FromJson(json);
+
+                Console.WriteLine(json);
+            }
+        }
+
+        private void ImageEditor_ImageSaved(object sender, Syncfusion.SfImageEditor.XForms.ImageSavedEventArgs args)
+        {
+            
+        }
+
+        private void ImageEditor_ImageEdited(object sender, Syncfusion.SfImageEditor.XForms.ImageEditedEventArgs e)
+        {
+            if(e.IsImageEdited)
+            {
+                
+            }
+        }
+
+        private void ImageEditor_ItemSelected(object sender, ItemSelectedEventArgs args)
+        {
+          
         }
     }
 }
