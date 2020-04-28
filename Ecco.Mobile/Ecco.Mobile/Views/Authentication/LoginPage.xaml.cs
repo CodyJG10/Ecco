0102e0b3348bd6fbb3ba3ba1373a0e7d762c5e8d
AcePilot10 <codyjg10@gmail.com>
2020-04-28 13:54:56 -0400

    added forgot password functionality

--- Ecco.Mobile/Ecco.Mobile/Views/Authentication/LoginPage.xaml.cs
+++ Ecco.Mobile/Ecco.Mobile/Views/Authentication/LoginPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Ecco.Mobile.ViewModels.Auth;
+using Syncfusion.XForms.Buttons;
+using Syncfusion.XForms.PopupLayout;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +18,61 @@ namespace Ecco.Mobile.Views.Authentication
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            string email = "";
+            var popupLayout = new SfPopupLayout();
+            popupLayout.PopupView.AnimationMode = AnimationMode.Zoom;
+            popupLayout.PopupView.ShowHeader = false;
+
+            var templateView = new DataTemplate(() =>
+            {
+                var popupContent = new Entry()
+                {
+                    Placeholder = "Email",
+                    HorizontalOptions = LayoutOptions.Center
+                };
+
+                popupContent.TextChanged += (obj, args) =>
+                {
+                    email = popupContent.Text;
+                };
+
+                return popupContent;
+            });
+
+            var footerTemplateView = new DataTemplate(() =>
+            {
+                var stackLayout = new StackLayout()
+                {
+                    Orientation = StackOrientation.Horizontal,
+                    HorizontalOptions = LayoutOptions.Center,
+                };
+
+                var yesButton = new SfButton()
+                {
+                    Text = "Submit",
+                    WidthRequest = 65d,
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                };
+
+                yesButton.Clicked += (s, a) =>
+                {
+                    (BindingContext as LoginPageViewModel).ForgotPasswordCommand.Execute(email);
+                    popupLayout.IsOpen = false;
+                };
+
+                stackLayout.Children.Add(yesButton);
+
+                return stackLayout;
+            });
+
+            popupLayout.PopupView.ContentTemplate = templateView;
+            popupLayout.PopupView.FooterTemplate = footerTemplateView;
+
+            popupLayout.Show();
         }
     }
 }
