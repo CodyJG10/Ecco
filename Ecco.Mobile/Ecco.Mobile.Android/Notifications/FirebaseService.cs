8457e0b7121b2f1bee3217b9a7f994933e6f032a
Cody Gordon <codyjg10@gmail.com>
2020-12-30 03:02:24 -0500

    updated ios images

--- Ecco.Mobile/Ecco.Mobile.Android/Notifications/FirebaseService.cs
+++ Ecco.Mobile/Ecco.Mobile.Android/Notifications/FirebaseService.cs
@@ -11,6 +11,7 @@ using Android.Runtime;
 using Android.Support.V4.App;
 using Android.Views;
 using Android.Widget;
+using AndroidX.Core.App;
 using Firebase.Messaging;
 using WindowsAzure.Messaging;
 
